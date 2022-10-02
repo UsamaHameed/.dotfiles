dc74f1703052e02060eac689414ec95e4d043d3e
usama <usamatech2@gmail.com>
2022-10-02 17:30:36 +0200

    i3

---
+++ i3/.config/i3_start.sh
@@ -0,0 +1,14 @@
+i3-msg "workspace 1; append_layout /home/usama/.config/i3/workspace-1.json"
+# exec discord
+exec firefox &>dev/null & disown 
+sleep 3
+
+gnome-terminal
+sleep 3
+
+i3-msg "workspace 2; append_layout /home/usama/.config/i3/workspace-2.json"
+
+exec thunderbird &>dev/null & disown
+sleep 3
+
+exec discord &>dev/null & disown
