bb2160d741f16e7f23ac61f182f460e9039922eb
usama <usamatech2@gmail.com>
2023-01-09 14:35:16 +0100

    add questions script

---
+++ bin/.local/scripts/1-1.js
@@ -0,0 +1,22 @@
+const https = require("https");
+
+const QUESTIONS_URL =
+  "https://raw.githubusercontent.com/VGraupera/1on1-questions/master/questions.json";
+const N = 5;
+
+const main = async () => {
+  https.get(QUESTIONS_URL, (res) => {
+    let text = "";
+    res.on("data", (chunk) => (text += chunk));
+    res.on("end", () => {
+      const questions = JSON.parse(text);
+      for (let i = 0; i < N; i++) {
+        console.log("\n", sample(questions).question);
+      }
+    });
+  });
+};
+
+const sample = (arr) => arr[Math.floor(arr.length * Math.random())];
+
+main();
