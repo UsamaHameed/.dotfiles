ac2cf92ebee03b44e86ef05c4a275e4d45c7cb9c
usama <usamatech2@gmail.com>
2023-02-05 23:01:09 +0100

    copy paste

--- nvim/.config/nvim/after/plugin/remap.lua
+++ nvim/.config/nvim/after/plugin/remap.lua
@@ -1,6 +1,7 @@
 local nnoremap = require("usama.keymap").nnoremap
 local inoremap = require("usama.keymap").inoremap
 local xnoremap = require("usama.keymap").xnoremap
+local vnoremap = require("usama.keymap").vnoremap
 
 nnoremap("<leader>pv", ":Ex<CR>")
 nnoremap("<leader><CR>", "<cmd>so ~/.config/nvim/init.lua<CR>")
@@ -38,4 +39,14 @@ nnoremap("<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 nnoremap("<leader>o", "o<Esc>")
 nnoremap("<leader>O", "O<Esc>")
+
+vnoremap("<leader>y",  "\"+y")
+nnoremap("<leader>Y",  "+yg_")
+nnoremap("<leader>y",  "+y")
+nnoremap("<leader>yy",  "+yy")
+
+nnoremap("<leader>p", "+p")
+nnoremap("<leader>P", "+P")
+vnoremap("<leader>p", "+p")
+vnoremap("<leader>P", "+P")
 
