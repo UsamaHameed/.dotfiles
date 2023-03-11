5b16284b8359dfe40ccc9acb1113be5e297aca74
usama <usamatech2@gmail.com>
2023-03-11 11:49:59 +0100

    fix telescrop

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,4 @@
-local telescope = require('telescope');
-local builtin = telescope.builtin;
+local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -7,6 +6,6 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>gm', telescope.extensions.git_worktree.create_git_worktree, {})
+-- vim.keymap.set('n', '<leader>gm', require('telescope').extensions.git_worktree.create_git_worktree, {})
 vim.keymap.set('n', '<leader>gd', builtin.diagnostics, {})
 
