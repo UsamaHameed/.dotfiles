27ebdc03f0f0c31fe418ca9cb46d4f2e18fad5a8
UsamaHameed <usamatech2@gmail.com>
2023-07-02 18:06:43 +0200

    typings for trouble.nvim

--- nvim/.config/nvim/after/plugin/trouble.lua
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -1,3 +1,16 @@
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+)
+
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
+
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+
